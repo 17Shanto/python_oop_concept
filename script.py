91d6022836c390c63db8ec808e1932e18708d1ee
17Shanto <shahporanshanto23@gmail.com>
2026-06-14 03:50:22 +0600

    basic class

--- script.py
+++ script.py
@@ -1,4 +1,26 @@
-name = "Danny"
-age = 29
+class Dog:
+    def __init__(self,name, breed, owner):
+        self.name = name
+        self.breed = breed
+        self.owner = owner
 
-print(name.upper())
+    def bark(self):
+        print("Whoof Whoof")
+
+
+class Owner:
+    def __init__(self, name, address, contact_number):
+        self.name = name
+        self.address = address
+        self.phone_number = contact_number
+
+
+
+owner1 = Owner("Danny","122 Springfield Drive", "888-999")
+dog1 = Dog("Bruce", "Scottish Terrier",owner1)
+
+print(dog1.name)
+print(dog1.breed)
+dog1.bark()
+
+print(dog1.owner.name)
